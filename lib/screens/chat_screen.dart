b6d055041b192ab34f2023e44acbca4ad4325589
Hanri <henry018@laposte.net>
2022-08-13 14:15:52 +0200

    MessagesWidget

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_chat/widgets/chat/messages.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({Key? key}) : super(key: key);
@@ -12,12 +13,13 @@ class ChatScreen extends StatelessWidget {
         title: const Text("Flutter Chat"),
         actions: [
           DropdownButton(
-            icon: const  Icon(Icons.more_vert),
+            icon: const Icon(Icons.more_vert, color: Colors.white,),
+            underline: Container(),
             items: [
               DropdownMenuItem(
                 value: "Log out",
                 child: Row(
-                  children: const [Icon(Icons.exit_to_app), SizedBox(width: 8,), Text("Log out")],
+                  children: const [Icon(Icons.exit_to_app, color: Colors.black,), SizedBox(width: 8,), Text("Log out")],
                 ),
               )
             ], 
@@ -29,23 +31,12 @@ class ChatScreen extends StatelessWidget {
           )
         ],
       ),
-      body: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-        stream: FirebaseFirestore.instance.collection("chats").snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            final texts = snapshot.data!.docs;
-            return ListView.builder(
-              padding: const EdgeInsets.all(8),
-              itemCount: snapshot.data!.size,
-              itemBuilder: (context, index) {
-                return Text(texts[index].data()["text"]);
-              },
-            );
-          }
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        }
+      body: Column(
+        children: const [
+          Expanded(
+            child: MessagesWidget()
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
---
+++ lib/widgets/chat/messages.dart
@@ -0,0 +1,28 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class MessagesWidget extends StatelessWidget {
+  const MessagesWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+      stream: FirebaseFirestore.instance.collection("chats").snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          final texts = snapshot.data!.docs;
+          return ListView.builder(
+            padding: const EdgeInsets.all(8),
+            itemCount: snapshot.data!.size,
+            itemBuilder: (context, index) {
+              return Text(texts[index].data()["text"]);
+            },
+          );
+        }
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      }
+    );
+  }
+}
