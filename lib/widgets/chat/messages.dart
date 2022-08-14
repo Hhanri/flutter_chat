521ddf161e45f1ce975ac523ce78de552193975b
Hanri <henry018@laposte.net>
2022-08-14 09:44:09 +0200

    FlutterFire_UI for firebase query optimisation

--- lib/widgets/chat/messages.dart
+++ lib/widgets/chat/messages.dart
@@ -2,34 +2,30 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chat/widgets/chat/message_bubble_widget.dart';
+import 'package:flutterfire_ui/firestore.dart';
 
 class MessagesWidget extends StatelessWidget {
   const MessagesWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-      stream: FirebaseFirestore.instance.collection("chats").orderBy("createdAt", descending: true).snapshots(),
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          final texts = snapshot.data!.docs;
-          return ListView.builder(
-            reverse: true,
-            padding: const EdgeInsets.all(8),
-            itemCount: snapshot.data!.size,
-            itemBuilder: (context, index) {
-              return MessageBubbleWidget(
-                key: ValueKey(texts[index].id),
-                message: texts[index].data()["text"],
-                isMe: texts[index].data()["uid"] == FirebaseAuth.instance.currentUser!.uid
-              );
-            },
+    return FirestoreListView<Map<String, dynamic>>(
+        padding: const EdgeInsets.all(8),
+        reverse: true,
+        shrinkWrap: true,
+        loadingBuilder: (context) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+        query: FirebaseFirestore.instance.collection("chats").orderBy("createdAt", descending: true),
+        itemBuilder: (context, snapshot) {
+          final Map<String, dynamic> doc = snapshot.data();
+          return MessageBubbleWidget(
+            key: ValueKey(snapshot.id),
+            message: doc["text"]!, isMe: doc["uid"] == FirebaseAuth.instance.currentUser!.uid
           );
         }
-        return const Center(
-          child: CircularProgressIndicator(),
-        );
-      }
     );
   }
 }
